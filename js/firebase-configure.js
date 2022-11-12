ab33e7e4a6e3d051e0d910411363f4f05861c3e5
saad ahmed <saadahmed1742@gmail.com>
2022-11-12 17:29:39 +0500

    completed student image in firebase

--- js/firebase-configure.js
+++ js/firebase-configure.js
@@ -1,7 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
 import { collection, query, where, onSnapshot, getFirestore, addDoc, doc,deleteDoc, updateDoc } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
-// https://firebase.google.com/docs/web/setup#available-libraries
+// import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js";
+import { getStorage , ref, uploadBytesResumable, getDownloadURL, uploadBytes } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-storage.js";
+
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyDmbBC5lLkqjKXAoA53QccTmyDCC11jA7c",
@@ -15,6 +18,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const auth = getAuth(app);
+export const storage = getStorage(app);
 export {signInWithEmailAndPassword}
 export {onSnapshot,collection, query, where, addDoc ,doc ,deleteDoc ,updateDoc}
+export {getStorage, ref, uploadBytesResumable, getDownloadURL, uploadBytes}
 
--- js/student.js
+++ js/student.js
@@ -1,65 +1,132 @@
-import {db,onSnapshot ,collection, query,addDoc,deleteDoc, doc} from "./firebase-configure.js"
+import {
+  db,
+  onSnapshot,
+  collection,
+  query,
+  addDoc,
+  deleteDoc,
+  doc,
+} from './firebase-configure.js'
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+  uploadBytes,
+  storage,
+} from './firebase-configure.js'
 
+let studentName = document.getElementById('studentName')
+let fatherName = document.getElementById('fatherName')
+let rollNumber = document.getElementById('rollNumber')
+let contactNumber = document.getElementById('contactNumber')
+let cnicNumber = document.getElementById('cnicNumber')
+let courseName = document.getElementById('courseName')
+let studentImage = document.getElementById('studentImage')
+let studentSubmitBtn = document.getElementById('studentSubmit')
 
-let studentName = document.getElementById("studentName");
-let fatherName = document.getElementById("fatherName");
-let rollNumber = document.getElementById("rollNumber");
-let contactNumber = document.getElementById("contactNumber");
-let cnicNumber = document.getElementById("cnicNumber");
-let courseName = document.getElementById("courseName");
-let studentImage = document.getElementById("studentImage");
-let studentSubmitBtn = document.getElementById("studentSubmit");
+// async function studentData() {
+//     await addDoc(collection(db, "students"), {
+//         studentname: studentName.value,
+//         studentFather: fatherName.value,
+//         rollNo: rollNumber.value,
+//         contact: contactNumber.value,
+//         cnic: cnicNumber.value,
+//         coursename: courseName.value,
+//       });
+//       studentName.value = "";
+//       fatherName.value = "";
+//       rollNumber.value = "";
+//       contactNumber.value = "";
+//       cnicNumber.value = "";
+//       courseName.value = "";
+// }
 
-
-
-async function studentData() {
-    await addDoc(collection(db, "students"), {
-        studentname: studentName.value,
-        studentFather: fatherName.value,
-        rollNo: rollNumber.value,
-        contact: contactNumber.value,
-        cnic: cnicNumber.value,
-        coursename: courseName.value,
-      });
-      studentName.value = "";
-      fatherName.value = "";
-      rollNumber.value = "";
-      contactNumber.value = "";
-      cnicNumber.value = "";
-      courseName.value = "";
-}
-studentSubmitBtn.addEventListener("click", studentData);
-let studentSection = document.getElementById("student-section");
+let studentSection = document.getElementById('student-section')
 
 function front() {
-
-   const q = query(collection(db, "students"));
-const unsubscribe = onSnapshot(q, (querySnapshot) => {
-    studentSection.innerHTML = ""
-  querySnapshot.forEach((doc) => {
-      console.log(doc.data());
-      console.log(doc.id)
-      studentSection.innerHTML += `
-      <ul class="course-section">
-      <li>${doc.data().studentname}</li>
-      <li>${doc.data().studentFather}</li>
-      <li>${doc.data().rollNo}</li>
-      <li>${doc.data().contact}</li>
-      <li>${doc.data().cnic}</li>
-      <li>${doc.data().coursename}</li>
-      <i class="fa-solid fa-pen-to-square" onclick="updateCourse('${doc.id}')"  id="edit" ></i>
-      <i class="fa-solid fa-trash" onclick="deletedStudent('${doc.id}')"></i>
-      </ul>
-      <br />
-      `
-  });
-});
-
+    const q = query(collection(db, 'students'))
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        studentSection.innerHTML = ''
+        querySnapshot.forEach((doc) => {
+            console.log(doc.data())
+            console.log(doc.id)
+            studentSection.innerHTML += `
+            <ul class="course-section">
+            <li>${doc.data().studentname}</li>
+            <li>${doc.data().studentFather}</li>
+            <li>${doc.data().rollNo}</li>
+            <li>${doc.data().contact}</li>
+            <li>${doc.data().cnic}</li>
+            <li>${doc.data().coursename}</li>
+            <i class="fa-solid fa-pen-to-square" onclick="updateCourse('${
+                doc.id
+            }')"  id="edit" ></i>
+            <i class="fa-solid fa-trash" onclick="deletedStudent('${doc.id}')"></i>
+            </ul>
+            <br />
+            `
+        })
+  })
 }
 front()
 
-
 async function deletedStudent(id) {
-    await deleteDoc(doc(db, "students", id));
+    await deleteDoc(doc(db, 'students', id))
 }
-window.deletedStudent = deletedStudent;
+window.deletedStudent = deletedStudent
+
+let modal = document.getElementById("modal");
+let userImageUrl;
+async function uploadPic() {
+    let file = studentImage.files[0]
+    let imageRef = ref(storage, `images/${file.name}`)
+    try {
+        let uploaded = await uploadBytes(imageRef, file)
+        userImageUrl = await getDownloadURL(imageRef)
+        console.log(userImageUrl, 'downloadable URL')
+        
+        await addDoc(collection(db, 'students'), {
+            studentname: studentName.value,
+            studentFather: fatherName.value,
+            rollNo: rollNumber.value,
+            contact: contactNumber.value,
+            cnic: cnicNumber.value,
+            coursename: courseName.value,
+            iMage: userImageUrl,
+        })
+    studentName.value = ''
+    fatherName.value = ''
+    rollNumber.value = ''
+    contactNumber.value = ''
+    cnicNumber.value = ''
+    courseName.value = ''
+    modal.style.display = "block";
+} catch (e) {
+    console.log(e)
+  }
+}
+studentSubmitBtn.addEventListener('click', uploadPic)
+
+// async function uploadPic(user) {
+//   let file = studentImage.files[0]
+//   let imageRef = ref(storage, `images/${file.name}`)
+//   try {
+//     let uploaded = await uploadBytes(imageRef, file)
+//     userImageUrl = await getDownloadURL(imageRef)
+//     console.log(userImageUrl, 'downloadable URL')
+
+//     await setDoc(doc(db, 'users', user), {
+//       name: fullName.value,
+//       email: signupEmail.value,
+//       iMage: userImageUrl,
+//     })
+//     fullName.value = ''
+//     signupEmail.value = ''
+//     signupPassword.value = ''
+//     signupConfirmPassword.value = ''
+//     window.location = '../pages/chatapp.html'
+//   } catch (e) {
+//     console.log(e)
+//   }
+// }
